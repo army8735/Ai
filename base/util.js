c030120d65deb38df60b5ab6a3f1a3f57e20fd37
army8735 <army8735@gmail.com>
2011-01-24 16:29:42 +0800

    增加格式化日期方法
    util中增加formatDate

--- base/util.js
+++ base/util.js
@@ -129,6 +129,37 @@
 				cb(false);
 			}
 		},
+		
+		/**
+		 * @public 格式化日期
+		 * @param {pattern} 格式化正则
+		 * @param {date} 需格式化的日期对象
+		 */
+		formatDate: function(pattern, date) {
+			function formatNumber(data, format) {
+				format = format.length;
+				data = data || 0;
+				return format == 1 ? data : (data = String(Math.pow(10, format) + data)).substr(data.length - format);
+			}
+			return pattern.replace(/([YMDhsm])\1*/g, function(format) {
+				switch(format.charAt()) {
+					case 'Y':
+						return formatNumber(date.getFullYear(), format);
+					case 'M':
+						return formatNumber(date.getMonth() + 1, format);
+					case 'D':
+						return formatNumber(date.getDate(), format);
+					case 'w':
+						return date.getDay() + 1;
+					case 'h':
+						return formatNumber(date.getHours(), format);
+					case 'm':
+						return formatNumber(date.getMinutes(), format);
+					case 's':
+						return formatNumber(date.getSeconds(), format);
+				}
+			});
+		},
 		/**
 		 * @public 根据data生成模板
 		 * @note 可以直接支持<script type="text/html">tpl</script>
