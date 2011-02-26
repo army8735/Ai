b14ec07094e58d654d28a0620faef9020e76d368
army8735 <army8735@gmail.com>
2011-02-26 22:59:21 +0800

    html5form
    大量更新，初步完成

--- plugin/html5form.js
+++ plugin/html5form.js
@@ -1,21 +1,110 @@
 (function() {
 
-	var body = $(document.body);
+	var body = $(document.body),
+		PLACE_HOLDER_CLASS = 'td_placeholder',
+		ERROR_CLASS = 'td_error',
+		TIP_CLASS = 'td_tip',
+		TYPE_VALID = {
+			'url': {
+				pattern: /^[a-zA-z]+:\/\/[^\s]*$/,
+				message: 'url格式不合法'
+			},
+			'email': {
+				pattern: /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/,
+				message: 'email格式不合法'
+			},
+			'number': {
+				pattern: /^\d+$/,
+				message: '这不是一个数字'
+			},
+			'date': {
+				pattern: /^\d{2,4}-\d{1,2}-\d{1,2}$/,
+				message: '日期格式不合法'
+			},
+			'time': {
+				pattern: /^\d{1,2}:\d{1,2}(:\d{1,2}(\.\d{1,3})?)?$/,
+				message: '时间格式不合法'
+			},
+			'color': {
+				pattern: /^#[a-z\d]{3,6}$/,
+				message: '颜色格式不合法'
+			}
+		},
+		tipBox;
 
 	function showError(node, message) {
-		var box = $('<div>').addClass('error').html(message).hide().appendTo(body),
-			left = node.offset().left + node.outerWidth() + 10,
-			top = node.offset().top;
-		if(left + box.outerWidth() > body.innerWidth()) {
-			left = body.innerWidth() - box.outerWidth();
-		}
+		var box = $('<div>').addClass(ERROR_CLASS).html(message).hide().appendTo(body),
+			pos = getPos(node, box);
+		//最初的坐标保存起来
+		box.data('left', pos.left).data('top', pos.top);
 		box.css({
-			left: left + 'px',
-			top: top + 'px'
-		}).slideDown(200).click(function() {
-			node.focus();
+			left: pos.left + 'px',
+			top: pos.top + 'px'
+		}).fadeIn(200).click(function() {
+			node.focus().val(node.val());
 		});
+		shake(box);
 		return box;
+	}
+	function hideError(box) {
+		clearShake(box);
+		box.remove();
+	}
+	function shake(box) {
+		clearShake(box);
+		var offset = 1,
+			count = 0;
+		//颤动动画存在自身的interval变量上
+		box.data('interval', setInterval(function() {
+			if(count++ > 10) {
+				clearShake(box);
+			}
+			box.css({
+				left: box.data('left') + offset + 'px',
+				top: box.data('top') + offset + 'px'
+			});
+			offset *= -1;
+		}, 50));
+	}
+	function clearShake(box) {
+		var interval = box.data('interval');
+		if(interval) {
+			clearInterval(interval);
+		}
+	}
+	function showTip(node, current, maxLength) {
+		if(!tipBox) {
+			tipBox = $('<div>').addClass(TIP_CLASS);
+		}
+		//focus时显示并设置位置
+		if(node) {
+			tipBox.hide().appendTo(body);
+			var pos = getPos(node, tipBox);
+			tipBox.css({
+				left: pos.left + 'px',
+				top: pos.top + 'px'
+			}).fadeIn(200);
+		}
+		//更新说明
+		current = current > maxLength ? '<strong>' + current + '</strong>' : current;
+		tipBox.html(current + ' / ' + maxLength);
+	}
+	function hideTip() {
+		if(tipBox) {
+			tipBox.html('').remove();
+		}
+	}
+	function getPos(node, box) {
+		var left = node.offset().left + node.outerWidth() + 10,
+			top = node.offset().top;
+		//不够放在node后面的时候，放在前面，top暂不考虑，很少遇到
+		if(left + box.outerWidth() > body.innerWidth()) {
+			left = Math.max(1, node.offset().left - box.outerWidth() - 10);
+		}
+		return {
+			left: left,
+			top: top
+		};
 	}
 
 	$.fn.html5form = function(cb) {
@@ -27,6 +116,7 @@
 				autofocus = 'autofocus' in input,
 				placeholder = 'placeholder' in input,
 				required = 'required' in input,
+				maxlength = 'maxlength' in input,
 				validArray = [];
 
 			//有novalidate属性的话无需验证表单
@@ -37,39 +127,42 @@
 			inputs.each(function(index) {
 				var item = $(this),
 					type = item.attr('type').toLowerCase(),
-					sourceType = this.getAttribute('type').toLowerCase();
+					sourceType = (this.getAttribute('type') || '').toLowerCase(),
+					interval;
 				//placeholder占位符
-				if(!placeholder && item.attr('placeholder')) {
+				if(!placeholder && this.getAttribute('placeholder') != null) {
 					var ph = item.attr('placeholder'),
-						place;
+						place; //开关，标明input当前是否是占位符状态。
+					//占位符为空字符串无效
 					if(ph.length) {
-						item.focus(function() {
+						function focus() {
 							//打开状态下认为是占位符
 							if(place) {
-								item.val('');
+								item.val('').removeClass(PLACE_HOLDER_CLASS);
 							}
-						});
-						item.blur(function() {
-							//离开时如有输入数据开关关闭，否则打开占位符
+						}
+						function blur() {
+							//离开时如有输入数据开关关闭，否则打开
 							if(item.val() == '') {
-								item.val(ph);
+								item.val(ph).addClass(PLACE_HOLDER_CLASS);
 								place = true;
 							}
 							else {
 								place = false;
 							}
-						});
-						item.blur();
-						//blur后判断一下，因为ie和ff会在刷新页面后遗留表单数据，此时占位符就成为遗留的默认数据了
-						if(ph == item.val()) {
+						}
+						item.focus(focus).blur(blur);
+						//初始化判断，因为ie和ff会在刷新页面后可能autocomplete遗留表单数据，此时占位符就成为遗留的默认数据；也可能在js执行前有用户输入。唯一的缺点是假如在js执行前用户输入的和占位符相同，会被误认为占位符，可忽视。
+						if(ph == item.val() || item.val() == '') {
 							place = true;
+							item.val(ph).addClass(PLACE_HOLDER_CLASS);
 						}
 					}
 				}
 				//autofocus自动聚焦
-				if(!autofocus && !$.isUndefined(item.attr('autofocus'))) {
+				if(!autofocus && this.getAttribute('autofocus') != null) {
 					item.focus();
-					//ie下需重新赋值使得光标至最后
+					//有遗留数据时ie下需重新赋值使得光标至最后
 					if(item.val().length && $.browser.msie) {
 						item.val(item.val());
 					}
@@ -83,41 +176,28 @@
 					});
 				}
 
-				//email类型，使用原生getAttribute可获取，否则不支持的浏览器一律为text
-				if(type == 'email' || sourceType == 'email') {
+				//默认的校验
+				var typeValid = TYPE_VALID[sourceType];
+				if(typeValid) {
 					item.blur(function() {
 						if(!validArray[index]) {
 							var v = item.val().trim();
-							if(v.length && !/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(v)) {
-								validArray[index] = showError(item, 'Email格式不合法');
+							if(v.length && !typeValid.pattern.test(v)) {
+								validArray[index] = showError(item, typeValid.message || '格式不正确');
 							}
 						}
 					});
 				}
-				//url类型
-				else if(type == 'url' || sourceType == 'url') {
-					item.blur(function() {
-						if(!validArray[index]) {
-							var v = item.val().trim();
-							if(v.length && !/[a-zA-z]+:\/\/[^\s]*/.test(v)) {
-								validArray[index] = showError(item, 'Url格式不合法');
-							}
-						}
-					});
-				}
-				//number类型
-				else if(type == 'number' || sourceType == 'number') {
+				//number类型另附验证范围
+				if(sourceType == 'number') {
 					var max = parseFloat(item.attr('max')),
 						min = parseFloat(item.attr('min'));
-					item.blur(function() {
-						if(!validArray[index]) {
-							var v = item.val().trim();
-							if(v.length) {
-								v = parseFloat(v);
-								if(isNaN(v)) {
-									validArray[index] = showError(item, '这不是一个数字');
-								}
-								else {
+					if(!isNaN(max) || !isNaN(min)) {
+						item.blur(function() {
+							if(!validArray[index]) {
+								var v = item.val().trim();
+								if(v.length) {
+									v = parseFloat(v);
 									if(!isNaN(max) && v > max) {
 										validArray[index] = showError(item, '超出范围，不能大于' + max);
 									}
@@ -126,53 +206,70 @@
 									}
 								}
 							}
-						}
-					});
+						});
+					}
 				}
-				//date类型
-				else if(type == 'date' || sourceType == 'date') {
+				//自定义pattern
+				var pattern = this.getAttribute('pattern');
+				if(pattern != null && pattern.length) {
+					pattern = new RegExp(pattern);
 					item.blur(function() {
 						if(!validArray[index]) {
 							var v = item.val().trim();
-							if(v.length && !/^\d{2,4}-\d{1,2}-\d{1,2}$/.test(v)) {
-								validArray[index] = showError(item, '日期格式不合法，正确例如：87-3-5');
+							if(v.length && !pattern.test(v)) {
+								validArray[index] = showError(item, '格式不正确');
 							}
 						}
 					});
 				}
-				//time类型
-				else if(type == 'time' || sourceType == 'time') {
-					item.blur(function() {
-						var v = item.val().trim();
-						if(v.length && !/^\d{1,2}:\d{1,2}(:\d{1,2}(\.\d{1,3})?)?$/.test(v)) {
-							validArray[index] = showError(item, '时间格式不合法，正确例如：8:7:3.5');
+				//maxlength
+				var maxLength = parseInt(this.getAttribute('maxlength'));
+				if(!isNaN(maxLength)) {
+					function input() {
+						showTip(null, item.val().length, maxLength);
+					}
+					item.focus(function() {
+						showTip(item, item.val().length, maxLength);
+					}).blur(function() {
+						hideTip();
+						var v = item.val().length;
+						if(!validArray[index] && v > maxLength) {
+							validArray[index] = showError(item, '最多只允许输入<strong>' + maxLength + '</strong>个字符');
 						}
 					});
+					//input事件除了ie都支持，可以用onpropertychange代替
+					if(window.addEventListener) {
+						this.addEventListener('input', input, false);
+					}
+					else if(window.attachEvent) {
+						this.attachEvent('onpropertychange', input);
+					}
 				}
+
 				//所有的:input聚焦时都要隐藏可能存在的错误提示框
 				item.focus(function() {
 					var error = validArray[index];
 					if(error) {
-						error.remove();
+						hideError(error);
 						validArray[index] = null;
 					}
 				});
 			});
 
-			//ie下侦听onpropertychange事件，标准侦听oninput
-			if(window.attachEvent) {
-				form.find(':input:visible:not(:button, :submit, :radio, :checkbox)').bind('propertychange', function() {
-					//
-				});
-			}
-			else {
-				form.delegate(':input:visible:not(:button, :submit, :radio, :checkbox)', 'input', function() {
-					//
-				});
-			}
-
 			form.submit(function() {
-				//
+				inputs.blur(); //全部触发可能存在的校验
+				var validResult = true;
+				validArray.forEach(function(item) {
+					if(item) {
+						validResult = false;
+						shake(item);
+					}
+				});
+				//本身通过html5校验，如有传入callback，返回callback的值
+				if(validResult && $.isFunction(cb)) {
+					validResult = cb.call(this) !== false;
+				}
+				return validResult;
 			});
 		});
 	}
