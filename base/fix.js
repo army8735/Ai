816562a89154a90dddf22710ae30979654927fee
army8735 <army8735@gmail.com>
2011-05-30 11:04:48 +0800

    更新compiler，优化fix
    fix中对ie下flash更改title和ie6不缓存背景图细微调整

--- base/fix.js
+++ base/fix.js
@@ -59,18 +59,19 @@
 		return $.trim(this);
 	};
 
-	$(function() {
-		//flash在ie下会更改title的bug
-		if($.browser.msie) {
-			var title = document.title;
-			$('object').live('mouseup', function(){
+	//flash在ie下会更改title的bug
+	if($.browser.msie) {
+		var title = document.title;
+		$(document).bind('mouseup', function(e) {
+			var n = e.target.nodeName;
+			if(({'EMBED': 1, 'OBJECT': 1})[n]) {
 				document.title = title;
-			});
-		}
+			}
+		});
 		//ie6缓存背景图
-		if($.browser.msie && $.browser.version == '6.0') {
+		if($.browser.version == '6.0') {
 			document.execCommand('BackgroundImageCache', false, true);
 		}
-	});
+	}
 
 })();
