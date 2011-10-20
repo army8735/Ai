7c294de60db96aea118433e081e342132fdf2ccc
unknown <army@.(none)>
2011-10-20 16:42:13 +0800

    drag
    add drag module

---
+++ plugin/drager.js
@@ -0,0 +1,171 @@
+define((function(options) {
+	var doc = $(document);
+	function setCoord(node, left, top) {
+		node.css({
+			left: left,
+			top: top
+		});
+	}
+	function Klass(options) {
+		this.options = options;
+		this.left = this.top = 0;
+		this.containers = [];
+		this.splitX = [];
+		this.splitY = [];
+		this.lastContainer = null;
+		this.tempContainer = null;
+		this.lastIndex = 0;
+		this.tempIndex = 0;
+		this._init();
+		this._event = new $$.Event();
+	}
+	Klass.prototype._reset = function() {
+		var self = this,
+			op = self.options,
+			container = $(op.container);
+		container.each(function(i, o) {
+			o = $(o);
+			//忽略被隐藏的容器
+			if(o.is(':hidden'))
+				return;
+			var drags = o.children(op.drag),
+				split = [];
+			drags.each(function(i2, o2) {
+				o2 = $(o2);
+				if(i2 > 0)
+					split.push(o2.offset().top);
+				if(i2 == drags.length - 1)
+					split.push(o2.offset().top + o2.outerHeight());
+			});
+			self.containers.push(o);
+			self.splitX.push(o.offset().left);
+			self.splitY.push(split);
+		});
+	}
+	Klass.prototype._init = function() {
+		var self = this,
+			op = self.options,
+			container = $(op.container);
+		container.delegate(op.handler, 'mousedown', function(e) {
+			self._reset();
+			var node = $(this).parent(op.drag),
+				position = node.css('position'),
+				left = node.css('left'),
+				right = node.css('right'),
+				con = node.parent();
+			//取得鼠标点下的坐标
+			self.left = node.offset().left - node.parent().offset().left;
+			self.top = node.offset().top - node.parent().offset().top;
+			//设置lastContainer和tempContainer为此节点的父容器
+			self.lastContainer = self.tempContainer = con[0];
+			//设置lastIndex为此节点在父容器中的次序
+			con.children(op.drag).each(function(i, o) {
+				if(node[0] == o) {
+					self.lastIndex = self.tempIndex = i;
+				}
+			});
+			//设置绝对定位和坐标
+			node.css('position', 'absolute');
+			setCoord(node, self.left, self.top);
+
+			doc.mousemove(move);
+			doc.mouseup(function(e) {
+				//还原节点最初的css属性
+				node.css('position', position);
+				setCoord(node, left, right);
+				//如果switch不设置为true，说明不是实时改变拖动块的位置，而是在drag:end再改变，需侦听
+				if(!op.switch) {
+					self._switch(node, $(self.tempContainer), self.tempIndex);
+				}
+				//解除事件侦听
+				doc.unbind('mousemove', move);
+				doc.unbind('mouseup', arguments.callee);
+				self._event.trigger('drag:end', [node]);
+			});
+			function move(evt) {
+				self._onMoveHandler(node, e, evt);
+				return false;
+			}
+			self._event.trigger('drag:start', [node]);
+			return false;
+		});
+	}
+	Klass.prototype._onMoveHandler = function(node, se, ne) {
+		var left = this.left + ne.pageX - se.pageX,
+			top = this.top + ne.pageY - se.pageY;
+		setCoord(node, left, top);
+		this._event.trigger('drag:move', [node]);
+		//检查是否发生了模块移动，先从左至右寻找容器
+		for(var i = 0, len = this.splitX.length; i < len; i++) {
+			if(this.splitX[i] >= ne.pageX) {
+				i--;
+				break;
+			}
+		}
+		//没有容器坐标值大于鼠标的x，设为最后一个容器，都大于的话，设为第一个
+		i = Math.max(0, i);
+		i = Math.min(i, len - 1);
+		var j = 0,
+			split = this.splitY[i];
+		len = split.length;
+		if(this.lastContainer == this.containers[i][0]) {
+			//从上至下寻找超过鼠标y的分割线的索引
+			for(; j < len; j++) {
+				if(split[j] >= ne.pageY) {
+					break;
+				}
+			}
+			//设置上下限
+			j = Math.max(0, j);
+			j = Math.min(j, len - 1);
+			//改变位置后才会触发
+			if(this.tempContainer != this.containers[i][0] || this.lastIndex != j) {
+				this.tempContainer = this.lastContainer;
+				this.lastIndex = this.tempIndex = j;
+				if(this.options.switch) {
+					this._switch(node, this.containers[i], j);
+				}
+				this._event.trigger('drag:switch', [node, this.containers[i], j]);
+			}
+		}
+		else {
+			for(; j < len; j++) {
+				if(split[j] >= ne.pageY) {
+					break;
+				}
+			}
+			//设置上下限
+			j = Math.max(0, j);
+			j = Math.min(j, len);
+			if((this.tempContainer == this.containers[i][0] && this.lastIndex != j) || this.tempContainer != this.containers[i][0]) {
+				this.tempContainer = this.containers[i][0];
+				this.lastIndex = this.tempIndex = j;
+				if(this.options.switch) {
+					this._switch(node, this.containers[i], j);
+				};
+				this._event.trigger('drag:switch', [node, this.containers[i], j]);
+			}
+		}
+	}
+	Klass.prototype._switch = function(node, container, i) {
+		if(i == 0)
+			container.prepend(node);
+		else {
+			var siblings = [];
+			container.children(this.options.drag).each(function(index, o) {
+				if(o != node[0]) {
+					siblings.push(o);
+				}
+			});
+			var before = $(siblings[i - 1]);
+			before.after(node);
+		}
+	}
+	Klass.prototype.bind = function(type, cb) {
+		this._event.bind(type, cb);
+	}
+	Klass.prototype.unbind = function(type, cb) {
+		this._event.unbind(type, cb);
+	}
+	return Klass;
+})());
