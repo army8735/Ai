c36cc4d8c011dd995cbd867fd7eef424f709c5b9
army8735 <army8735@gmail.com>
2011-11-16 12:11:08 +0800

    2^ delay
    *32 改为 <<5

--- Ai.js
+++ Ai.js
@@ -400,7 +400,7 @@
 							delay = true;
 							if(delayCount > 4)
 								throw new Error('2^ delay mode is too long to wait');
-							setTimeout(d2, Math.pow(2, delayCount++) * 32);
+							setTimeout(d2, Math.pow(2, delayCount++) << 5);
 						}
 					}
 				});
